ff67ed26eb83250726e8cc6d382456ef112247f1
Erik Saule <esaule@charlotte.edu>
2025-07-30 13:09:12 -0400

    linechart is not called tut_

--- tutorials/testing/scripts/setting_bridgespublic.sh
+++ tutorials/testing/scripts/setting_bridgespublic.sh
@@ -103,7 +103,7 @@ FORCE_BRIDGES_ASSIGNMENT=10170 ./tut_graph_p3 9999 fakename fakeapi
 
 ## tutorials added : 7/19/22
 
-FORCE_BRIDGES_ASSIGNMENT=10180 ./tut_linechart 9999 fakename fakeapi
+FORCE_BRIDGES_ASSIGNMENT=10180 ./linechart 9999 fakename fakeapi
 
 ## tutorials added : 7/19/24
 
